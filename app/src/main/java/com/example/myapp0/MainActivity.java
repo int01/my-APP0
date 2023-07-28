deb453c3c650710ab43ff38ca36904515bd82998
minhongwei <minhongwei@wiseasy.com>
2023-07-28 10:52:40 +0800

    register

--- app/src/main/java/com/example/myapp0/MainActivity.java
+++ app/src/main/java/com/example/myapp0/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.myapp0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Binder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapp0.activity.LoginActivity;
+import com.example.myapp0.activity.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginBtn;
+    private Button registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.btn_login);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in =new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(in);
+            }
+        });
+        registerBtn = findViewById(R.id.btn_register);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
